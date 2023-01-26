51135047020e64f4f995550d916b62570db3ad23
acifre <78503928+acifre@users.noreply.github.com>
2023-01-25 21:14:04 -0600

    Add prediction functionality to calculateBedtime()

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -7,13 +7,17 @@
 
 // https://www.youtube.com/watch?v=a905KIBw1hs CREATE ML FOR EVERYONE
 
+import CoreML
 import SwiftUI
+
 
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     @State private var wakeUp = Date.now
+    
+    
     
     
     var body: some View {
@@ -41,6 +45,23 @@ struct ContentView: View {
     
     func calculateBedtime() {
         
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+        } catch {
+            
+        }
+        
+
+        
     }
 }
 
@@ -50,3 +71,5 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
+
+
