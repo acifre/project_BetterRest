b46465206c33e782c0e8d3560997571d34782b04
acifre <78503928+acifre@users.noreply.github.com>
2023-01-25 20:59:05 -0600

    Create basic UI and logic

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,14 +5,42 @@
 //  Created by Anthony Cifre on 1/24/23.
 //
 
+// https://www.youtube.com/watch?v=a905KIBw1hs CREATE ML FOR EVERYONE
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    @State private var wakeUp = Date.now
+    
+    
     var body: some View {
-        VStack {
-            Text("BetterRest")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Pick Date:", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) \(coffeeAmount > 1 ? "cups" : "cup")", value: $coffeeAmount, in: 1...20)
+            
+            }
+            .padding()
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
-        .padding()
+    }
+    
+    func calculateBedtime() {
+        
     }
 }
 
