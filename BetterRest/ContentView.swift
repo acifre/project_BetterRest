832d3d635af297a87c8c79c4d81fd20d0c5504fc
acifre <78503928+acifre@users.noreply.github.com>
2023-01-25 21:24:33 -0600

    Add defaultWakeupTime computed property

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -15,14 +15,26 @@ struct ContentView: View {
     
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     
     
     
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
                 Text("When do you want to wake up?")
                     .font(.headline)
                 DatePicker("Pick Date:", selection: $wakeUp, displayedComponents: .hourAndMinute)
@@ -40,6 +52,12 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
+
         }
     }
     
@@ -56,10 +74,17 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
             let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
+            
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
             
         }
         
+        showingAlert = true
 
         
     }
